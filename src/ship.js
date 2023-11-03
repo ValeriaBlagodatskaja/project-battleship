0e2debf24dd0aae4ae22bffbbacc349cb322cdf8
Valeria48562 <vblagodatskaja@gmail.com>
2023-11-03 12:17:19 +0200

    created and tested ship factory function

---
+++ src/ship.js
@@ -0,0 +1,42 @@
+//length object
+const SHIP_LENGTHS = {
+  carrier: 5,
+  battleship: 4,
+  cruiser: 3,
+  submarine: 3,
+  destroyer: 2,
+};
+
+const ship = (type) => {
+  const id = type;
+  const length = SHIP_LENGTHS[type];
+  let direction = "horizontal";
+
+  let hits = 0;
+
+  //hit
+  const hit = () => {
+    if (hits < length) {
+      // You can hit a ship only if it hasn't been sunk
+      hits++;
+    }
+  };
+
+  const getHitNum = () => hits;
+  //isSunk
+  const isSunk = () => {
+    if (hits >= length) {
+      return true;
+    }
+    return false;
+  };
+
+  const getDirection = () => direction;
+  const changeDirection = () => {
+    direction = direction === "horizontal" ? "vertical" : "horizontal";
+  };
+
+  return { id, length, hit, getHitNum, isSunk, getDirection, changeDirection };
+};
+
+export default ship;
