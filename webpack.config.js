ef0d10ac82c072aee2201d4435272d1873198d4d
Valeria48562 <vblagodatskaja@gmail.com>
2023-11-15 06:36:58 +0200

    webpack setup is changed

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,28 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
     }),
   ],
   devServer: {
     open: true,
     hot: true,
-    watchFiles: ['src/**/*'],
+    watchFiles: ["src/**/*"],
   },
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: ['node_modules'],
-        use: ['babel-loader'],
-      },
-      {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.gif$/,
-        type: 'asset/inline',
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
-    ],
-  },
-  resolve: {
-    alias: {
-      config$: './configs/app-config.js',
-      react: './vendor/react-master',
-    },
-    extensions: ['.js', '.jsx'],
-    modules: [
-      'node_modules',
-      'bower_components',
-      'shared',
-      '/shared/vendor/modules',
     ],
   },
 };
